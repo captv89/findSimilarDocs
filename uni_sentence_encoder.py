8880899bcae96cf654acfd8a9664b7a13bd708f8
captv89 <techsavvysailor@gmail.com>
2024-01-02 09:54:35 +0100

    read and write from milvus, search still  buggy

--- uni_sentence_encoder.py
+++ uni_sentence_encoder.py
@@ -1,23 +1,37 @@
-import tensorflow as tf
 import tensorflow_hub as hub
-from pymilvus import Milvus, DataType
+from pymilvus import CollectionSchema, FieldSchema, DataType, Collection, utility, connections
 
 # Connect to Milvus server
-milvus = Milvus(host='localhost', port='19530')
+connections.connect(
+  alias="default",
+  user='username',
+  password='password',
+  host='localhost',
+  port='19530'
+)
 
-# Create a collection (analogous to a table in a traditional database)
-collection_name = 'document_embeddings'
-milvus.create_collection(collection_name, {'dimension': 512}, data_type=DataType.FLOAT_VECTOR)
+# Define PrimaryKey field
+primary_field = FieldSchema(name="id", dtype=DataType.INT64, is_primary=True, auto_id=True)
+
+# Define field schema for document vectors
+document_name_field = FieldSchema(name="document_name", dtype=DataType.VARCHAR, max_length=200,
+  # The default value will be used if this field is left empty during data inserts or upserts.
+  # The data type of `default_value` must be the same as that specified in `dtype`.
+  default_value="Unknown")
+embedding_field = FieldSchema(name="embedding", dtype=DataType.FLOAT_VECTOR, dim=512)
+
+# Create a collection schema
+schema = CollectionSchema(fields=[primary_field, document_name_field, embedding_field], description="Collection for storing document vectors")
+
+# Specify collection parameters
+collection_name = "document_embeddings"
+shards_num = 2
+
+# Create a collection
+collection = Collection(name=collection_name, schema=schema, using='default', shards_num=shards_num)
 
 
-
-# From online hub
-# module_url = "https://tfhub.dev/google/universal-sentence-encoder/4"
-# To download the model url + ?tf-hub-format=compressed
-
-# model = hub.load(module_url)
-# print("module %s loaded" % module_url)
-
+# Load model
 # From modal available locally
 model_path = 'universal-sentence-encoder_4'
 model = hub.load(model_path)
@@ -26,13 +40,27 @@ model = hub.load(model_path)
 def embed(input):
     return model(input)
 
-def embed_and_add_to_collection(input, collection_name):
-    embeddings = embed(input)
+def embed_and_add_to_collection(document_data, collection_name):
+    collection = Collection(collection_name)
+    embeddings = embed([content for _, content in document_data])
+    
+    # Convert embeddings to list
     vectors = [embedding.numpy().tolist() for embedding in embeddings]
-    milvus.insert(collection_name=collection_name, records=vectors)
+    
+    # Get document names
+    doc_names = [doc_name for doc_name, _ in document_data]
+    
+    data = [
+        doc_names,
+        vectors
+    ]
+
+    status = collection.insert(data)
+    print(status)
+    
 
 # Load target document
-target_document = open('document1.txt').read()
+target_document = open('document3.txt').read()
 
 # Load collection of word documents along with their names
 document_data = [
@@ -46,36 +74,50 @@ document_data = [
 # Represent documents as vectors
 
 # Embed each document in the collection
-embed_and_add_to_collection([document for _, document in document_data], collection_name)
+embed_and_add_to_collection(document_data, collection_name)
+
+
+# Build Index
+index_params = {
+  "metric_type":"L2",
+  "index_type":"IVF_FLAT",
+  "params":{"nlist":1024}
+}
+
+# Get an existing collection.
+collection = Collection(collection_name)      
+collection.create_index(
+  field_name="embedding", 
+  index_params=index_params
+)
+
+utility.index_building_progress(collection_name)
 
 # Embed target document
 target_document_embedding = embed([target_document])[0]
 
-# print(type(target_document_embedding))
-# <class 'tensorflow.python.framework.ops.EagerTensor'>
-
-# Calculate cosine similarity between target document and each document in the collection
-cos_scores = []
-# for i, (doc_name, doc_embedding) in enumerate(zip(document_data, document_embeddings)):
-#     cos_similarity = tf.keras.losses.cosine_similarity(target_document_embedding, doc_embedding)
-#     cos_scores.append((doc_name[0], cos_similarity.numpy()))
-
-# Check similarity between target document and each document in the collection
-target_document_embedding_numpy = target_document_embedding.numpy()
+# Convert to numpy array
+target_document_list = [target_document_embedding.numpy().tolist()]
 
 # Search for similar documents
 top_k = 5
 search_param = {
     'nprobe': 16
 }
-status, results = milvus.search(collection_name=collection_name, query_records=[target_document_embedding_numpy], top_k=top_k, params=search_param)
 
-# Print results
-for result in results[0]:
-    print(result.id, result.distance)
+# Search in collection
+collection = Collection(collection_name)
+collection.load()
+
+# Prepare search parameters
+search_params = {
+    "metric_type": "L2",
+    "params": {"nprobe": 16}
+}
+
+results = collection.search(data=target_document_list, anns_field="embedding", param=search_params, limit=top_k, expr=None, output_fields=['document_name'], consistency_level="Strong")
+
+hit = results[0]
+print(hit)
 
 
-# # Print document name and percentage similarity
-# for doc_name, similarity_score in cos_scores:
-#     percentage_similarity = abs(similarity_score) * 100
-#     print(f"Document: {doc_name}, Similarity: {percentage_similarity:.2f}%")
